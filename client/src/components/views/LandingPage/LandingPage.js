c100e5a89a14a9521ffc152ff4e49fb35fe755ef
Jae Hyun Nam <nam2350@gmail.com>
2020-11-27 13:10:40 +0900

    first

---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+
+function LandingPage() {
+    return (
+            <div>
+                
+            </div>
+    )
+}
+
+export default LandingPage
